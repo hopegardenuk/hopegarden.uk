bd91a4ba105b6653f9cafee86614f3ac7319fff8
jaker <hi@codeberg.grwd.uk>
2025-02-17 19:38:41 +0000

    delete tina directory

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '24b1f6ea533ee37ba3acab36c267748b626eb469', queries,  });
-export default client;
-  
